2d85def7db4cb305211706380c755681796265bf
koseokhyeon1187 <koseokhyn@gmail.com>
2026-05-20 20:47:48 +0900

    Update api.js

--- vecel/src/components/front/api.js
+++ vecel/src/components/front/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || '';
+const BASE_URL = 'http://3.34.115.198:8080'; 
 
 export const fetchWithAuth = async (url, options = {}) => {
   let accessToken = sessionStorage.getItem('accessToken');
